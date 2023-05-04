64325bb3202fa507dce87b26859638c9196a0dee
MitsuKensy <apollodosol50@gmail.com>
2023-05-04 12:20:15 -0300

    aulinha

---
+++ Atividade/atividade.js
@@ -0,0 +1,89 @@
+function comprar(){
+
+    total.alert = ``
+
+    let id_lactinio = lactinio.value;
+
+    let id_carne = carne.value;
+    let id_limpeza = limpeza.value;
+    let id_eletrônicos = eletrônicos.value;
+    let id_doces = doces.value;
+    
+    switch (id_lactinio){
+
+
+            case "1":
+        alert ("monitor");
+            break;
+            
+            case "2":
+        alert ("gabinete");
+            break;
+            
+            case "3":
+        alert ("caixa de som");
+            break;
+    }
+
+    switch (id_carne){
+
+            case "1":
+        alert ("monitor");
+            break;
+            
+            case "2":
+        alert ("gabinete");
+            break;
+            
+            case "3":
+        alert ("caixa de som");
+            break;
+    }
+
+    switch (id_eletrônicos){
+
+            case "1":
+        alert ("monitor");
+            break;
+            
+            case "2":
+        alert ("gabinete");
+            break;
+            
+            case "3":
+        alert ("caixa de som");
+            break;
+    }
+
+    switch (id_limpeza){
+
+
+            case "1":
+        alert ("monitor");
+            break;
+            
+            case "2":
+        alert ("gabinete");
+            break;
+            
+            case "3":
+        alert ("caixa de som");
+            break;
+    }
+
+    switch (id_doces){
+
+
+            case "1":
+        alert ("monitor");
+            break;
+            
+            case "2":
+        alert ("gabinete");
+            break;
+            
+            case "3":
+        alert ("caixa de som");
+            break;
+    }
+}
